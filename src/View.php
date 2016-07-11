2c6230e30991baf15832602ba73f451e8fb209fd
docker <dock3r@gmail.com>
2016-07-11 15:19:44 +0200

    Add View component

---
+++ src/View.php
@@ -0,0 +1,67 @@
+<?php
+namespace cncYTC;
+
+class View {
+
+	private $data = array();
+
+	function __construct()
+	{
+		add_shortcode('cnc_yt_channel', [$this, 'shortcodeChannelPlayer']);
+	}
+
+	/**
+	 * Assign new value to data stack
+	 * @param  string $variable Variable name
+	 * @param  mixed $value    Value of the variable
+	 */
+	public function assign($variable, $value)
+	{
+		$this->data[$variable] = $value;
+	}
+
+	/**
+	 * Render content using the given template file
+	 * @param  string $template Template file name without extension
+	 * @return string           Generated HTML markup
+	 */
+	public function render($template)
+	{
+		extract($this->data);
+		$file = CNC_TEMPLATE_DIR . CNC_DS . $template . '.tpl.php';
+		if (!file_exists($file)) {
+			throw new \Exception("File doesn't exist");
+		}
+		ob_start();
+		include($file);
+		return ob_get_clean();
+	}
+
+	/**
+	 * Trim given text to specified length
+	 * @param  string $string   Text to trim
+	 * @param  int $length   Length in characters
+	 * @param  string $replacer Pattern to end with
+	 * @return string           Trimmed text
+	 */
+	public function limitString($string, $length, $replacer = '...') {
+	  if (strlen($string) > $length)
+	     $string = mb_substr($string, 0, $length-3) . $replacer;
+	  return $string;
+	}
+
+	/**
+	 * Generate channel player shortcode
+	 * @param  array $args Given attributes
+	 */
+	public function shortcodeChannelPlayer($args)
+	{
+	    // extract the attributes into variables
+	    $atts = shortcode_atts(array(
+	        'num' => -1,
+	    ), $args);
+
+	    $this->assign('', $events);
+	    return $this->render('');
+	}
+}
