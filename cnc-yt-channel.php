86289aab4e84ea906f19c69cc64ba00d61838bf6
docker <dock3r@gmail.com>
2016-07-11 13:51:11 +0200

    plugin base

---
+++ cnc-yt-channel.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Plugin Name: CNC Youtube channel player
+Plugin URI: http://colorandcode.hu
+Description: Youtube channel player shortcode
+Version: 1.0
+Author: docker
+Author URI: https://hu.linkedin.com/in/docker
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// load translations
+load_plugin_textdomain( 'cnc-yt-channel-newsletter', false, 'cnc-yt-channel/languages' );
+
+/**
+ * Initial settings
+ */
+define('CNC_DS', DIRECTORY_SEPARATOR);
+define('CNC_PROJECT_PATH', realpath(dirname(__FILE__)));
+define('CNC_PROJECT_URL', plugins_url() . CNC_DS . 'cnc-yt-channel');
+define('CNC_TEMPLATE_DIR', CNC_PROJECT_PATH . CNC_DS . 'templates');
+define('CNC_THEME', get_stylesheet_directory());
+
+/**
+ * Autoload
+ */
+$vendorAutoload = CNC_PROJECT_PATH . CNC_DS . 'vendor' . CNC_DS . 'autoload.php';
+if (is_file($vendorAutoload)) {
+	require_once($vendorAutoload);
+}
---
+++ config.sample.php
@@ -0,0 +1,5 @@
+<?php
+return array (
+	'channel_id' => 'Channel ID here', // Youtube channel ID
+	'api_key' => 'API key here', // Youtube API key
+);
