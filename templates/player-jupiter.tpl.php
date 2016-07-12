7a977e3355b3ea731b6b3866cc013564edfb70da
docker <dock3r@gmail.com>
2016-07-12 14:29:35 +0200

    Add player style

--- templates/player-jupiter.tpl.php
+++ templates/player-jupiter.tpl.php
@@ -1,9 +1,9 @@
-<div class="youtube_player shortcode-block">
-  <div class="col-sm-6 col-md-8" id="featured_video">
+<div class="youtube_player shortcode-block mk-grid">
+  <div class="mk-col-8-12" id="featured_video">
     	<div class="signal"></div>
     	<div class="video-container row"><?php echo $video_featured; ?></div>
   </div>
-  <div class="col-sm-6 col-md-4">
+  <div class="mk-col-4-12">
     <?php echo $video_list; ?>
     <a class="more-yt-videos" href="https://www.youtube.com/user/TransparencyHU" target="_blank">
       <?php _e('More', 'cnc-yt-channel'); ?></a>
