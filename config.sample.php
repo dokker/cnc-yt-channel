d837b563cbd5bb5066fd497159da91cfb1a34e08
docker <dock3r@gmail.com>
2016-07-12 14:32:42 +0200

    Add default template config option

--- config.sample.php
+++ config.sample.php
@@ -2,4 +2,5 @@
 return array (
 	'channel_id' => 'Channel ID here', // Youtube channel ID
 	'api_key' => 'API key here', // Youtube API key
+	'player_template' => 'player-bootstrap', // Channel player template name
 );
